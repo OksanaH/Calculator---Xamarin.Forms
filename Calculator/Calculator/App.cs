a14bce251d8b20e21470a8ea4537c1fff477ccd1
Oxie <oxieking83@gmail.com>
2018-07-07 14:04:15 +0100

    Added static resource file with some properties set there; added xamlc

--- Calculator/Calculator/App.cs
+++ Calculator/Calculator/App.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
+/*Compliler performs compile-time checking of XAML, notifying the user of any errors.
+  removes some of the load and instantiation time for XAML elements.
+  helps to reduce the file size of the final assembly by no longer including .xaml files.*/
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Calculator
 {
 	public class App : Application
 	{
+        
 		public App()
 		{
 			// The root page of your application
---
+++ Calculator/Calculator/SharedResources.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public static class SharedResources
+    {
+        public static Color OpButtonBkColor
+        {
+            get { return Color.FromRgb(0xff, 0xa5, 0); }
+        }
+        public static Font FontSize36
+        {
+            get { return Font.OfSize("Arial", 36); }
+        }
+    }
+}
